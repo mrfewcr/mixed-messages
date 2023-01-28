9d3e9d254ef4aba454b06b4af129a2c2627c2834
Mr Few <dperezc@Davids-MacBook-Pro.local>
2023-01-28 14:37:02 -0600

    first commit, code ready

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mixed Messages
+A fun and short Codecademy project built with JavaScript that generates random messages when the client types "yes hello".
+
+## Technologies
+- JavaScript
+- Node for running locally
+
+## Setup
+To run this project, run node.js on your terminal while you're at the directoryw here the file is at.
---
+++ script.js
@@ -0,0 +1,43 @@
+let randomMessage = Math.floor(Math.random()*3);
+let randomCity = Math.floor(Math.random()*6);
+let randomQuantity = Math.floor(Math.random()*6);
+let randomPeople = Math.floor(Math.random()*6);
+
+let incompleteMessages = [
+    [", oh very nice","it rains ","you should visit with "],
+    [" is waiting for you","it has great food, ","bring "],
+    [", the city of carnaval","you may come ","call "]
+];
+
+let mixedCities = [
+    "Curitiba",
+    "Londrina",
+    "Rio Grande do Sul",
+    "São Paulo",
+    "Recife",
+    "Joinville"
+]
+
+let mixedQuantities = [
+    "a lot",
+    "rarely",
+    "always",
+    "every now and then",
+    "only in July",
+    "superb"
+]
+
+let mixedPeople = [
+    "your family",
+    "your wife",
+    "your parents",
+    "your friends",
+    "your class",
+    "yourself"
+]
+
+let mixedMessage = `${mixedCities[randomCity]}${incompleteMessages[randomMessage][0]} \n
+${incompleteMessages[randomMessage][1]}${mixedQuantities[randomQuantity]}\n
+${incompleteMessages[randomMessage][2]}${mixedPeople[randomPeople]}`;
+
+console.log(mixedMessage);
